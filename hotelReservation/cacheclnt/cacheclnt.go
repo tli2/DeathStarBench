01a3a03e3b2534309479b583fef655ee7a10f6d7
Ariel Szekely <arielck@mit.edu>
2023-05-02 22:33:37 -0400

    x

--- hotelReservation/cacheclnt/cacheclnt.go
+++ hotelReservation/cacheclnt/cacheclnt.go
@@ -1,9 +1,9 @@
 package cacheclnt
 
 import (
+	"log"
 	"net/http"
 	"net/rpc"
-  "log"
 	"sync"
 	"sync/atomic"
 
@@ -11,17 +11,17 @@ import (
 )
 
 const (
-  CACHE_CLNT_PORT = ":9999"
+	CACHE_CLNT_PORT = ":9999"
 )
 
 type CacheClnt struct {
 	mu  sync.Mutex
 	ccs []cached.CachedClient
-  ncs int32
+	ncs int32
 }
 
 func MakeCacheClnt() *CacheClnt {
-c := &CacheClnt{
+	c := &CacheClnt{
 		ccs: make([]cached.CachedClient, 0),
 	}
 
@@ -32,15 +32,14 @@ c := &CacheClnt{
 		log.Fatalf("Error Listen in Coordinator.registerServer: %v", err)
 	}
 	go http.Serve(l, nil)
-	return c 
-  }
+	return c
+}
 
-  func (c *CacheClnt) Get(key string) ([]byte, bool) {
-  }
+func (c *CacheClnt) Get(key string) ([]byte, bool) {
+}
 
-  func (c *CacheClnt) Set(key string, b []byte) {
-  }
-
+func (c *CacheClnt) Set(key string, b []byte) {
+}
 
 type RegisterCacheRequest struct {
 	Addr string
@@ -50,28 +49,28 @@ type RegisterCacheResponse struct {
 	OK bool
 }
 
-func (c *CacheClnt) RegisterCache(req RegisterCacheRequest*, rep *RegisterCacheResponse) error {
+func (c *CacheClnt) RegisterCache(req *RegisterCacheRequest, rep *RegisterCacheResponse) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-  // Make a deep copy of the client slice, so we can atomically swap it with
-  // the existing slice. This way, clients don't have to take a lock on the
-  // slice when executing RPCs.
-  ccs := make([]cached.CachedClient, len(c.ccs))
-  for i := range ccs {
-    ccs[i] =  c.ccs[i]
-  }
-  // Append the new client.
-  ccs = append(ccs, dialClient(req.Addr))
-  // Swap in the new slice, which should be done atomically.
-  c.ccs = ccs
-  // Atomically increase the number by which we mod when selecting a shard.
-  atomic.AddInt32(&c.ncs, 1)
+	// Make a deep copy of the client slice, so we can atomically swap it with
+	// the existing slice. This way, clients don't have to take a lock on the
+	// slice when executing RPCs.
+	ccs := make([]cached.CachedClient, len(c.ccs))
+	for i := range ccs {
+		ccs[i] = c.ccs[i]
+	}
+	// Append the new client.
+	ccs = append(ccs, dialClient(req.Addr))
+	// Swap in the new slice, which should be done atomically.
+	c.ccs = ccs
+	// Atomically increase the number by which we mod when selecting a shard.
+	atomic.AddInt32(&c.ncs, 1)
 	return nil
 }
 
 func dialClient(addr string) cached.CachedClient {
-  // TODO
-  log.Fatalf("Figure out how to dial client")
-  return nil
+	// TODO
+	log.Fatalf("Figure out how to dial client")
+	return nil
 }
